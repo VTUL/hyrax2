0fde5096eaa76ece50d610a62957d4b7bc7f52bc
Tingting Jiang <tvj@vt.edu>
2018-05-16 17:53:39 +0000

    LIBTD-1255: Add basic metadata, item model, form, indexer, and presenter
    Customize edit fields, show fields, auto-filled fields
    
    Customize medium, type, and tags metadata based on controlled vocabs
    
    Use correct labels in forms/views and show metadata fields in facet/index/show for Item
    
    Fix facet label for keyword and date_range label
    
    Remove duplicated facet label

---
+++ app/models/concerns/hyrax/basic_metadata.rb
@@ -0,0 +1,55 @@
+module Hyrax
+  # An optional model mixin to define some simple properties. This must be mixed
+  # after all other properties are defined because no other properties will
+  # be defined once  accepts_nested_attributes_for is called
+  module BasicMetadata
+    extend ActiveSupport::Concern
+
+    included do
+      property :label, predicate: ActiveFedora::RDF::Fcrepo::Model.downloadFilename, multiple: false
+
+      property :relative_path, predicate: ::RDF::URI.new('http://scholarsphere.psu.edu/ns#relativePath'), multiple: false
+
+      property :import_url, predicate: ::RDF::URI.new('http://scholarsphere.psu.edu/ns#importUrl'), multiple: false
+      property :resource_type, predicate: ::RDF::Vocab::DC11.type
+      property :creator, predicate: ::RDF::Vocab::DC11.creator
+      property :contributor, predicate: ::RDF::Vocab::DC11.contributor
+      property :description, predicate: ::RDF::Vocab::DC11.description
+      property :keyword, predicate: ::RDF::Vocab::DC11.relation
+      # Used for a license
+      property :license, predicate: ::RDF::Vocab::DC.rights
+      property :rights, predicate: ::RDF::Vocab::DC11.rights, multiple: false do |index|   
+        index.as :stored_searchable
+      end
+      # This is for the rights statement
+      property :rights_statement, predicate: ::RDF::Vocab::EDM.rights
+      property :publisher, predicate: ::RDF::Vocab::DC11.publisher
+      property :date_created, predicate: ::RDF::Vocab::DC.created, multiple: false do |index|
+        index.as :stored_searchable
+      end
+      property :date, predicate: ::RDF::Vocab::DC11.date, multiple: false do |index|
+        index.as :stored_searchable
+      end
+      property :subject, predicate: ::RDF::Vocab::DC11.subject
+      property :language, predicate: ::RDF::Vocab::DC11.language
+      property :identifier, predicate: ::RDF::Vocab::DC11.identifier
+      property :based_near, predicate: ::RDF::Vocab::FOAF.based_near, class_name: Hyrax::ControlledVocabularies::Location
+      property :related_url, predicate: ::RDF::RDFS.seeAlso
+      property :bibliographic_citation, predicate: ::RDF::Vocab::DC.bibliographicCitation, multiple: false do |index|
+        index.as :stored_searchable
+      end
+      property :coverage, predicate: ::RDF::Vocab::DC11.coverage do |index|
+        index.as :stored_searchable, :facetable
+      end
+      property :source, predicate: ::RDF::Vocab::DC11.source
+      property :rights_holder, predicate: ::RDF::Vocab::DC.rightsHolder, multiple: false  do |index|
+        index.as :stored_searchable
+      end
+      id_blank = proc { |attributes| attributes[:id].blank? }
+
+      class_attribute :controlled_properties
+      self.controlled_properties = [:based_near]
+      accepts_nested_attributes_for :based_near, reject_if: id_blank, allow_destroy: true
+    end
+  end
+end
--- app/presenters/hyrax/item_presenter.rb
+++ app/presenters/hyrax/item_presenter.rb
@@ -2,5 +2,21 @@
 #  `rails generate hyrax:work Item`
 module Hyrax
   class ItemPresenter < Hyrax::WorkShowPresenter
+    delegate :date, :coverage, :location, :medium, :bibliographic_citation,
+             :rights, :rights_holder, :format, :contributor, to: :solr_document
+
+    def attribute_to_html(field, options = {})
+      unless respond_to?(field)
+        Rails.logger.warn("#{self.class} attempted to render #{field}, but no method exists with that name.")
+        return
+      end
+      if :date == field.to_sym
+        value = send(:date_created)
+        value += " - #{send(field)}" unless send(field).nil?
+        renderer_for(field, options).new(field, value, options).render
+      else
+        renderer_for(field, options).new(field, send(field), options).render
+      end
+    end
   end
 end
